e0e4c0605988a8069d69f6d1a9e9aadc4f202341
Troy Unverdruss <troy.unverdruss@vevo.com>
2018-09-04 17:46:49 -0700

    Initial Commit

---
+++ Classic Computer Science Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Classic Computer Science Swift
+//
+//  Created by Troy Unverdruss on 9/4/18.
+//  Copyright © 2018 Troy Unverdruss. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
